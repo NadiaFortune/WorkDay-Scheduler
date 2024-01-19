a0301c5a19c58185d095e743b2bff1502b256ca5
NadiaFortune <hello@nadiafortune.co.uk>
2024-01-19 19:53:35 +0000

    function for save button and local storage created

--- starter/script.js
+++ starter/script.js
@@ -1,13 +1,4 @@
-//Luxon - date library for JS
-//date-fns - modern JS date utility library
-//Day.js (for use in broweser)
-//Moment.js
-//
 
-
-
- 
-// * Present timeblocks for standard business hours when the user scrolls down.
  
 // * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
  
@@ -22,9 +13,34 @@
 // reference the whole task (time and textarea)
 var timeBlockEl = document.querySelector('.container');
 
-// Display the current day at the top of the calender when a user opens the planner.
+// Display the current day at the top of the calender when a user opens the planner:
 var today = dayjs().format("dddd, DD MMMM YYYY")
 $("#currentDay").text(today); 
+
+
+// * Present timeblocks for standard business hours when the user scrolls down.
+
+//Create Event Listener for 'Save' Button:
+$('.saveBtn').on('click', function () {
+    // get nearby values of the description in jQuery
+    var textValue = $(this).siblings('.description').val();
+    // get the id attribute of the parent div element
+    var timeKey = $(this).parent().attr('id');
+  
+    // save in local storage
+  localStorage.setItem(timeKey, textValue);
+});
+
+// Get item from local storage if any
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
 
 
 
