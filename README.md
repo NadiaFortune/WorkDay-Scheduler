8f8c226980bb32497bde8ce37530ab1ad2a00c72
NadiaFortune <hello@nadiafortune.co.uk>
2024-01-23 21:38:01 +0000

    final code added

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # WorkDay-Scheduler
+
+## **Description**
+
+I created a simple workday scheduler to enable a user to record their daily activity/schedule.
+
+The desired outcome was to allow for the following to happen::
+
+-Display the current day at the top of the calender when a user opens the planner.
+-Show timeblocks for standard business hours when the user scrolls down.
+-Each timeblock would be become color-coded according to whether an event was in past, present, or future when the timeblock is viewed.
+-Allow a user to enter an event when they click a timeblock
+-Save the event in local storage when the save button is clicked in that timeblock
+-Persist events between refreshes of a page
+
+Languages ued to create the applictaion were:
+-HTML
+-CSS
+-Vanilla JavaScript
+-JQuery, incorporating day.js
+
+
+The deployed page can be found here: https://nadiafortune.github.io/Password-Generator/
+
+A screenshot of the deployed page is below:  
+
+![](assets/Screenshot%202024-01-07%20153814.png)
+
+
+## **Installation**
+
+The application runs within the window and is initiated when the user clicks the 'Generate Password' button.
+
+
+## **Usage**
+
+N/A
+
+## **Credits**
+
+The following resources were used to research the coding funcionality for the successful deployment of the application: YouTube, w3schools, and MDN.
+
+I did not use a tutor guidance session for this challenge.
+
+## **Licence**
+
+Please refer to the LICENCE in the repo.
+
+
--- starter/script.js
+++ starter/script.js
@@ -1,46 +1,67 @@
-
- 
-// * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
- 
-// * Allow a user to enter an event when they click a timeblock
-
-// * Save the event in local storage when the save button is clicked in that timeblock.
-
-// * Persist events between refreshes of a page
-
-
-
 // reference the whole task (time and textarea)
-var timeBlockEl = document.querySelector('.container');
+var timeBlockEl = document.querySelector('.container')
 
 // Display the current day at the top of the calender when a user opens the planner:
-var today = dayjs().format("dddd, DD MMMM YYYY")
-$("#currentDay").text(today); 
+var today = dayjs().format("dddd, DD MMMM YYYY, H:mm")
+$("#currentDay").text(today)
 
-
-// * Present timeblocks for standard business hours when the user scrolls down.
-
+// Allow a user to enter an event when they click a timeblock
 //Create Event Listener for 'Save' Button:
 $('.saveBtn').on('click', function () {
     // get nearby values of the description in jQuery
-    var textValue = $(this).siblings('.description').val();
+    var textValue = $(this).siblings('.description').val()
     // get the id attribute of the parent div element
-    var timeKey = $(this).parent().attr('id');
+    var timeKey = $(this).parent().attr('id')
   
-    // save in local storage
-  localStorage.setItem(timeKey, textValue);
-});
 
-// Get item from local storage if any
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$('#hour17 .description').val(localStorage.getItem('hour17'));
+// * Save the event in local storage when the save button is clicked in that timeblock and // * Persist events between refreshes of a page
+  localStorage.setItem(timeKey, textValue)
+})
+
+$("#hour9 .description").val(localStorage.getItem("hour9"))
+$("#hour10 .description").val(localStorage.getItem("hour10"))
+$("#hour11 .description").val(localStorage.getItem("hour11"))
+$("#hour12 .description").val(localStorage.getItem("hour12"))
+$("#hour13 .description").val(localStorage.getItem("hour13"))
+$("#hour14 .description").val(localStorage.getItem("hour14"))
+$("#hour15 .description").val(localStorage.getItem("hour15"))
+$("#hour16 .description").val(localStorage.getItem("hour16"))
+$('#hour17 .description').val(localStorage.getItem('hour17'))
+
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+function auditTask() {
+  // get current number of hours
+  var now = dayjs().hour()
+
+  // loop over each time block
+  $(".time-block").each(function () {
+    var timeId = parseInt($(this).attr("id").split("hour")[1])
+
+    console.log(timeId, now)
+    // if the time Id attribute is before the current hour, add the past class
+    if (timeId < now) {
+      $(this).addClass("past")
+    } // if the time Id attribute is equal to the current hour, remove the past and future classes and add the present class
+    else if (timeId === now) {
+      $(this).removeClass("past")
+      $(this).removeClass("future")
+      $(this).addClass("present")
+    } // If the time Id attribute is greater than the current time, remove the past and present classes and add the future class
+    else {
+      $(this).removeClass("past")
+      $(this).removeClass("present")
+      $(this).addClass("future")
+    }
+  })
+}
+// Call the audit task function
+auditTask()
+
+// Use setTimeout to update the time every minute (1000ms * 60s)
+setTimeout(function () {
+  // clear the current URL
+  location = "" // location references the current URL
+}, 1000 * 60)
 
 
 
